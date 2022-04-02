375103e6f633f67e362fd737726460c4167ef9b9
nivetha <nivetha@santech.com>
2022-04-02 20:40:28 +0530

    update code

--- name.py
+++ name.py
@@ -1,7 +1,7 @@
 import numpy as np
 from flask import Flask, render_template,request
 import pickle#Initialize the flask App
-app = Flask(__name__)
+app = Flask(__name__,template_folder="templates")
 model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/')
 def home():
